1f45de1ef9412060787101d6b4061e2ebd7a221c
starship9 <nishanksaini10@gmail.com>
2017-03-11 19:51:37 +0530

    Added individual shapes for each cluster

--- kMeans.R
+++ kMeans.R
@@ -1,16 +1,20 @@
 kc<-read.table("classifieddataNew.txt")
+
 dim(kc)
 head(kc)
 library(ggplot2)
-kc<-kc[1:100,]
-ggplot(kc, aes(V1, V2, color = V3)) + geom_point()
-set.seed(20)
-kCluster<-kmeans(kc,2,nstart=20)
+kc1<-kc[1:1000,]
+View(kc)
+ggplot(kc1, aes(V1, V2, color = V3)) + geom_point()
+#set.seed(10)
+kCluster<-kmeans(kc1,3,nstart=10)
+kCluster
 kCluster$cluster <- as.factor(kCluster$cluster)
-ggplot(kc, aes(V1, V2, color = kCluster$cluster)) + geom_point()
+ggplot(kc1, aes(V1, V2, color = kCluster$cluster, shape = kCluster$cluster)) + geom_point()
+#?kmeans
 
 library(cluster)
 library(fpc)
-plotcluster(kc, kCluster$cluster)
-clusplot(kc, kCluster$cluster, color=TRUE, shade=TRUE,labels=2, lines=0)
+plotcluster(kc1, kCluster$cluster)
+clusplot(kc1, kCluster$cluster, color=TRUE, shade=TRUE,labels=2, lines=0)
 
